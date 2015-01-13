edc78ffbc31cb8d4fe22807c4b10986b39ff2a46
oscarrel <oscarrela@yahoo.es>
2015-01-13 12:52:51 +0100

    Actualizacion post-navidad

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$('#registro').validate({
+    onkeyup: false,
+    onfocusout: false,
+    onclick: false,
+    rules: {
+    	dpes_02: "required",
+      dpes_03: "required",
+      dpes_05: "required",
+      dpes_06: "required",
+      fnac_01: "required",
+      fnac_02: "required",
+      fnac_03: "required",
+      dpes_10: "required",
+      email: "required",
+      emailConf: "required",
+      dcia_02: "required",
+      dcia_0511: "required",
+      addr_010: "required",
+      addr_020: "required",
+      addr_041: "required",
+      addr_051: "required",
+      addr_0610: "required"
+
+      }
+});
